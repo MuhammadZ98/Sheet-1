77a19a7f03588933d027907ce4e59a66d4c419bf
MuhammadZ98 <44571546+MuhammadZ98@users.noreply.github.com>
2019-03-09 21:11:10 +0200

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,22 +1,21 @@
 package com.company;
-import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int F1 = 0, F2 = 1, Fn = 0;
-        int n = in.nextInt();
+import java.math.BigInteger;
+
+public class Fibonacci {
+    static BigInteger fibonacci(int n){
+        BigInteger F0 = new BigInteger("0");
+        BigInteger F1 = new BigInteger("1");
+        BigInteger Fn = new BigInteger("1");
         if(n == 0){
+            Fn = F0;
+        } else if (n == 1) {
             Fn = F1;
-        } else if(n == 1){
-            Fn = F2;
         } else if(n > 1) {
-            for (int i = 1; i < n; i++) {
-                Fn = F1 + F2;
-                F1 = F2;
-                F2 = Fn;
-            }
+            Fn = F0.add(F1);
+            F0 = F1;
+            F1 = Fn;
         }
-        System.out.println(Fn);
+        return Fn;
     }
 }
